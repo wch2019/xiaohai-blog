54d3d0dc5358c05863f686e35ce0293ad08f3d2e
wangchenghai <1372195290@qq.com>
2023-02-15 20:54:39 +0800

    ConfigMapper

---
+++ xiaohai-system/src/main/java/com/xiaohai/system/dao/ConfigMapper.java
@@ -0,0 +1,16 @@
+package com.xiaohai.system.dao;
+
+import com.xiaohai.system.pojo.entity.Config;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 系统配置 Mapper 接口
+ * </p>
+ *
+ * @author xiaohai
+ * @since 2023-02-01
+ */
+public interface ConfigMapper extends BaseMapper<Config> {
+
+}
