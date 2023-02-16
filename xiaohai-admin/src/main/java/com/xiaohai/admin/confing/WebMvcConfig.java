a6a10eacc9c49e35359fe6cc84e3d5a6a5d4416c
wangchenghai <1372195290@qq.com>
2023-02-16 22:55:11 +0800

    开启跨域

--- xiaohai-admin/src/main/java/com/xiaohai/admin/confing/WebMvcConfig.java
+++ xiaohai-admin/src/main/java/com/xiaohai/admin/confing/WebMvcConfig.java
@@ -4,6 +4,7 @@ import cn.dev33.satoken.interceptor.SaInterceptor;
 import cn.dev33.satoken.stp.StpUtil;
 import com.xiaohai.admin.confing.intercept.PageableInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,5 +25,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/login","/sendEmailCode","/register");
         //分页拦截器
         registry.addInterceptor(new PageableInterceptor());
+    }
+    /**
+     * 开启跨域
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路由
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许证书（cookies）
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
     }
 }
