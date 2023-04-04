b19092d96dcc0473714b0e46404424b43824d3f5
wangchenghai <1372195290@qq.com>
2023-04-04 10:54:30 +0800

    修改全局滚动条

--- xiaohai-web/src/App.vue
+++ xiaohai-web/src/App.vue
@@ -9,3 +9,26 @@ export default {
   name: 'App'
 }
 </script>
+
+<style lang="scss">
+
+::-webkit-scrollbar {
+  /*滚动条整体样式*/
+  width: 10px; /*高宽分别对应横竖滚动条的尺寸*/
+  height: 1px;
+}
+
+::-webkit-scrollbar-thumb {
+  /*滚动条里面小方块*/
+  border-radius: 10px;
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  background: #535353;
+}
+
+::-webkit-scrollbar-track {
+  /*滚动条里面轨道*/
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  background: rgba(234, 233, 233, 0.98);
+}
+</style>
