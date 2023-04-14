323c580cf026fba1c81afd9f0275a81c62ae7604
wangchenghai <1372195290@qq.com>
2023-04-14 19:48:05 +0800

    主页调整

--- xiaohai-web/src/views/dashboard/admin/components/PanelGroup.vue
+++ xiaohai-web/src/views/dashboard/admin/components/PanelGroup.vue
@@ -7,7 +7,7 @@
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            New Visits
+            用户数
           </div>
           <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
         </div>
@@ -20,7 +20,7 @@
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            Messages
+            评论数
           </div>
           <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />
         </div>
@@ -29,11 +29,11 @@
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
       <div class="card-panel" @click="handleSetLineChartData('purchases')">
         <div class="card-panel-icon-wrapper icon-money">
-          <svg-icon icon-class="money" class-name="card-panel-icon" />
+          <svg-icon icon-class="eye-open" class-name="card-panel-icon" />
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            Purchases
+            浏览量
           </div>
           <count-to :start-val="0" :end-val="9280" :duration="3200" class="card-panel-num" />
         </div>
@@ -42,17 +42,18 @@
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
       <div class="card-panel" @click="handleSetLineChartData('shoppings')">
         <div class="card-panel-icon-wrapper icon-shopping">
-          <svg-icon icon-class="shopping" class-name="card-panel-icon" />
+          <svg-icon icon-class="edit" class-name="card-panel-icon" />
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            Shoppings
+            文章数
           </div>
           <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
         </div>
       </div>
     </el-col>
   </el-row>
+
 </template>
 
 <script>
--- xiaohai-web/src/views/dashboard/index.vue
+++ xiaohai-web/src/views/dashboard/index.vue
@@ -1,46 +1,76 @@
 <template>
   <div class="dashboard-container">
-    <iframe src="http://192.168.68.207:7576" style="height:1000px;width: 100%;"/>
+    <!--    <iframe src="http://192.168.68.207:7576" style="height:1000px;width: 100%;"/>-->
     <div class="dashboard-text">name: {{ name }}</div>
-    <iframe
-      src="http://192.168.68.207:7576?graph=cpu&showPercentage=true&theme=light"
-      style="border-radius: 20px"
-      allowtransparency="true"
-      frameborder="0"
-    />
-    <iframe
-      src="http://192.168.68.207:7576?graph=ram&showPercentage=true&theme=light"
-      style="border-radius: 20px"
-      allowtransparency="true"
-      frameborder="0"
-    />
-    <iframe
-      src="http://192.168.68.207:7576?graph=network&showPercentage=true&theme=light"
-      style="border-radius: 20px"
-      allowtransparency="true"
-      frameborder="0"
-    />
-    <iframe
-      src="http://192.168.68.207:7576?graph=storage&multiView=true&showPercentage=true&theme=light"
-      style="border-radius: 20px"
-      allowtransparency="true"
-      frameborder="0"
-    />
-    <!--    <component :is="currentRole" />-->
-  </div>
+    <!--    <iframe-->
+    <!--      src="http://192.168.68.207:7576?graph=cpu&showPercentage=true&theme=light"-->
+    <!--      style="border-radius: 20px"-->
+    <!--      allowtransparency="true"-->
+    <!--      frameborder="0"-->
+    <!--    />-->
+    <!--    <iframe-->
+    <!--      src="http://192.168.68.207:7576?graph=ram&showPercentage=true&theme=light"-->
+    <!--      style="border-radius: 20px"-->
+    <!--      allowtransparency="true"-->
+    <!--      frameborder="0"-->
+    <!--    />-->
+    <!--    <iframe-->
+    <!--      src="http://192.168.68.207:7576?graph=network&showPercentage=true&theme=light"-->
+    <!--      style="border-radius: 20px"-->
+    <!--      allowtransparency="true"-->
+    <!--      frameborder="0"-->
+    <!--    />-->
+    <!--    <iframe-->
+    <!--      src="http://192.168.68.207:7576?graph=storage&multiView=true&showPercentage=true&theme=light"-->
+    <!--      style="border-radius: 20px"-->
+    <!--      allowtransparency="true"-->
+    <!--      frameborder="0"-->
+    <!--    />-->
+    <component :is="currentRole" />
+    <el-card class="box-card" shadow="hover" style="width: 100%;">
+      <div slot="header">
+        <span style="text-align: center">文章数</span>
+      </div>
+      <calendar-heatmap
+        :end-date="new Date().toLocaleDateString()"
+        :values="timeValue"
+        :locale="locale"
+        tooltip-unit="文章数"
+        :max="6"
+      />
+      近一年提交
+    </el-card>
 
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import adminDashboard from './admin'
+import 'vue-calendar-heatmap/dist/vue-calendar-heatmap.css'
+import { CalendarHeatmap } from 'vue-calendar-heatmap/dist/vue-calendar-heatmap.common'
 
 export default {
   name: 'Dashboard',
-  components: { adminDashboard },
+  components: { adminDashboard, CalendarHeatmap },
   data() {
     return {
-      currentRole: 'adminDashboard'
+      currentRole: 'adminDashboard',
+      locale: {
+        months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        days: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+        on: '：',
+        less: '少',
+        more: '多'
+      },
+      timeValue: [
+        { date: '2023-04-02', count: 1 },
+        { date: '2023-04-02', count: 2 },
+        { date: '2023-04-03', count: 3 },
+        { date: '2023-04-04', count: 4 },
+        { date: '2023-04-05', count: 5 },
+        { date: '2023-04-06', count: 6 }
+      ]
     }
   },
   computed: {
