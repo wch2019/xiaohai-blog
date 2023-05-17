df677221b8ba67f802562005ccfb6f19c4d5402b
wangchenghai <1372195290@qq.com>
2023-05-17 20:26:46 +0800

    请求时添加token

--- xiaohai-web-show/src/utils/axios.ts
+++ xiaohai-web-show/src/utils/axios.ts
@@ -1,5 +1,6 @@
 import Axios from 'axios'
 import { ElMessage } from 'element-plus'
+import { getToken } from '@/utils/auth'
 
 const axios = Axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -8,6 +9,14 @@ const axios = Axios.create({
 // 前置拦截器（发起请求之前的拦截）
 axios.interceptors.request.use(
   (response) => {
+    const newResponse = { ...response }
+    // 在发送请求之前执行某些操作
+    if (getToken()) {
+      // let each request carry token
+      // ['X-Token'] is a custom headers key
+      // please modify it according to the actual situation
+      newResponse.headers.authorization = getToken()
+    }
     /**
      * 根据你的项目实际情况来对 config 做处理
      * 这里对 config 不做任何处理，直接返回
