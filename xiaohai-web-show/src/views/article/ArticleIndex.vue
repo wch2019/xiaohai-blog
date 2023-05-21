c5e6b8d1d6f06084b96b8856a686694fba25dfbe
wangchenghai <1372195290@qq.com>
2023-05-21 23:07:50 +0800

    评论test

--- xiaohai-web-show/src/views/article/ArticleIndex.vue
+++ xiaohai-web-show/src/views/article/ArticleIndex.vue
@@ -261,24 +261,24 @@ const like = (id: string, finish: () => void) => {
 }
 
 config.comments = [
-  {
-    id: '1',
-    parentId: null,
-    uid: '1',
-    address: '来自上海',
-    content:
-      '缘生缘灭，缘起缘落，我在看别人的故事，别人何尝不是在看我的故事?别人在演绎人生，我又何尝不是在这场戏里?谁的眼神沧桑了谁?我的眼神，只是沧桑了自己[喝酒]',
-    likes: 2,
-    contentImg: 'https://gitee.com/undraw/undraw-ui/raw/master/public/docs/normal.webp',
-    createTime: dayjs().subtract(10, 'minute').toString(),
-    user: {
-      username: '落🤍尘',
-      avatar:
-        'https://static.juzicon.com/avatars/avatar-200602130320-HMR2.jpeg?x-oss-process=image/resize,w_100',
-      level: 6,
-      homeLink: '/1'
-    }
-  }
+  // {
+  //   id: '1',
+  //   parentId: null,
+  //   uid: '1',
+  //   address: '来自上海',
+  //   content:
+  //     '缘生缘灭，缘起缘落，我在看别人的故事，别人何尝不是在看我的故事?别人在演绎人生，我又何尝不是在这场戏里?谁的眼神沧桑了谁?我的眼神，只是沧桑了自己[喝酒]',
+  //   likes: 2,
+  //   contentImg: 'https://gitee.com/undraw/undraw-ui/raw/master/public/docs/normal.webp',
+  //   createTime: dayjs().subtract(10, 'minute').toString(),
+  //   user: {
+  //     username: '落🤍尘',
+  //     avatar:
+  //       'https://static.juzicon.com/avatars/avatar-200602130320-HMR2.jpeg?x-oss-process=image/resize,w_100',
+  //     level: 6,
+  //     homeLink: '/1'
+  //   }
+  // }
 ]
 
 const { queryParams } = toRefs(data)
