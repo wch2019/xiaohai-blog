a63a32261025748ab21ed04278097bcf94d73f8a
wangchenghai <1372195290@qq.com>
2023-06-23 14:46:31 +0800

    不可删除当前登录用户bug修复

--- xiaohai-system/src/main/java/com/xiaohai/system/service/impl/UserServiceImpl.java
+++ xiaohai-system/src/main/java/com/xiaohai/system/service/impl/UserServiceImpl.java
@@ -81,9 +81,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     @Transactional(rollbackFor = Exception.class)
     public Integer delete(Long[] ids) {
         // 当前操作用户
-        Object userId = StpUtil.getLoginId();
+        Long userId = Long.valueOf((String) StpUtil.getLoginId());
         for (Long id : ids) {
-            Assert.isTrue(Objects.equals(userId, id), "不可删除当前登录用户");
+            Assert.isTrue(userId!=id, "不可删除当前登录用户");
             //删除角色
             userRoleService.delete(Math.toIntExact(id));
             //删除用户
