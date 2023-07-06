eacd3370dbcee0a6a1df26483a14daf38e2109e1
wangchenghai <1372195290@qq.com>
2023-07-06 15:23:18 +0800

    添加接口文档展示页

---
+++ xiaohai-web/src/views/api/swagger/index.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <div class="container">
+    <iframe class="iframe" :src="url+'/swagger-ui.html'" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      url: process.env.VUE_APP_BASE_API
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+  .container {
+    position: absolute;
+    top: 50px;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 95%;
+    border: none;
+  }
+</style>
