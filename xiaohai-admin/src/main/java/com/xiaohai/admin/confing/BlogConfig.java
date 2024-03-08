818e80902264bb68e593ee84540cbd5cd1602a94
wangchenghai <1372195290@qq.com>
2024-03-08 10:21:37 +0800

    ...

---
+++ xiaohai-admin/src/main/java/com/xiaohai/admin/confing/BlogConfig.java
@@ -0,0 +1,24 @@
+package com.xiaohai.admin.confing;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wangchenghai
+ * @date 2024/03/07 15:08:35
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "blog")
+public class BlogConfig {
+    /**
+     * 项目名称
+     */
+    private String name;
+
+    /**
+     * 版本
+     */
+    private String version;
+}
