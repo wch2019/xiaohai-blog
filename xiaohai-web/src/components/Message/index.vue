2a833f9d7bc1fef8d16491ea6f5d63aa085af2e9
wangchenghai <1372195290@qq.com>
2024-03-20 23:04:00 +0800

    消息中心点赞添加分页

--- xiaohai-web/src/components/Message/index.vue
+++ xiaohai-web/src/components/Message/index.vue
@@ -1,37 +1,45 @@
 <template>
   <div class="app-container">
-    <el-card>
-      <div slot="header" class="clearfix">
-        <span>消息中心</span>
-      </div>
-      <el-tabs v-model="activeName" tab-position="left" :stretch="true" style="height: calc(100vh - 190px);">
-        <el-tab-pane v-for="item in $store.getters.dict.sys_notification_type" :key="item.name" :label="item.dictLabel" :name="item.dictValue">
-          <div v-for="like in alertsLike" v-show="activeName==='1'" :key="like.id">
-            <el-badge v-if="like.isRead===0" is-dot class="item" style="position: absolute;right: 0;" />
-            <div class="content">
-              <div style="display: flex;justify-content: space-between;">
-                <div class="content-flex">
-                  <el-image :src="header(like.likeDto?like.likeDto.avatar:'')" />
-                  <div class="name-header">
-                    <div class="name">{{ like.likeDto?like.likeDto.nickName:'' }}</div>
-                    <div class="subhead">
-                      <span>赞了你的文章</span>
-                      <span>{{ like.createdTime }}</span>
-                    </div>
-                    <el-tooltip :content="like.title" placement="bottom">
-                      <el-link class="ellipsis-link" :underline="false" @click="onClick(like)">
-                        《{{ like.title }}》
-                      </el-link>
-                    </el-tooltip>
+    <!--    <el-card>-->
+    <!--      <div slot="header" class="clearfix">-->
+    <!--        <span>消息中心</span>-->
+    <!--      </div>-->
+    <el-tabs v-model="activeName" tab-position="left" :stretch="true" class="tabs" @tab-click="handleClick">
+      <el-tab-pane v-for="item in $store.getters.dict.sys_notification_type" :key="item.name" :label="item.dictLabel" :name="item.dictValue">
+        <div v-for="like in alertsLike" v-show="activeName==='1'" :key="like.id">
+          <el-badge v-if="like.isRead===0" is-dot class="item" style="position: absolute;right: 0;" />
+          <div class="content">
+            <div style="display: flex;justify-content: space-between;">
+              <div class="content-flex">
+                <el-image :src="header(like.likeDto?like.likeDto.avatar:'')" />
+                <div class="name-header">
+                  <div class="name">{{ like.likeDto?like.likeDto.nickName:'' }}</div>
+                  <div class="subhead">
+                    <span>赞了你的文章</span>
+                    <span>{{ like.createdTime }}</span>
                   </div>
+                  <el-tooltip :content="like.title" placement="bottom">
+                    <el-link class="ellipsis-link" :underline="false" @click="onClick(like)">
+                      《{{ like.title }}》
+                    </el-link>
+                  </el-tooltip>
                 </div>
-                <el-button v-if="like.isRead===0" type="text" class="know-button" @click="handleKnowClick(like)">我知道了</el-button>
               </div>
+              <el-button v-if="like.isRead===0" type="text" class="know-button" @click="handleKnowClick(like)">我知道了</el-button>
             </div>
           </div>
-        </el-tab-pane>
-      </el-tabs>
-    </el-card>
+        </div>
+      </el-tab-pane>
+      <pagination
+        v-show="total>0"
+        :total="total"
+        style=" bottom: 20px; right: 20px;  position: fixed;"
+        :page.sync="params.pageNum"
+        :limit.sync="params.pageSize"
+        @pagination="getUnreadList"
+      />
+    </el-tabs>
+    <!--    </el-card>-->
   </div>
 </template>
 
@@ -44,11 +52,15 @@ export default {
   data() {
     return {
       url: process.env.VUE_APP_BLOG_WEB_API,
+      // 总条数
+      total: 0,
       alertsLike: [],
       alertsCommon: [],
       alertsSystem: [],
       activeName: '1',
       params: {
+        pageNum: 1,
+        pageSize: 10,
         type: '1'
       }
     }
@@ -60,6 +72,7 @@ export default {
     getUnreadList() {
       this.params.type = this.activeName
       listNotifications(this.params).then(response => {
+        this.total = response.data.total
         this.alertsLike = response.data.records
         this.alertsCommon = response.data.records
         this.alertsSystem = response.data.records
@@ -146,6 +159,13 @@ export default {
 
   .content:hover .know-button {
     display: inline-block; /* 鼠标悬停时显示按钮 */
+  }
+
+  .tabs{
+    height: calc(100vh - 190px);
+  }
+  .tabs:hover{
+    overflow-y: auto;
   }
 
 </style>
