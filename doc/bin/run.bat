f0f8af975dbd046728ff24592094fe3eaf6887a3
wangchenghai <1372195290@qq.com>
2024-04-21 23:01:57 +0800

    修改文档

--- doc/bin/run.bat
+++ doc/bin/run.bat
@@ -5,15 +5,15 @@ rem 设置 Java 可执行文件的路径
 set JAVA_HOME=C:\Program Files\Java\jdk-17.0.3.1
 set PATH=%JAVA_HOME%\bin;%PATH%
 rem 设置 JAR 文件的完整路径
-set JAR_FILE=D:\Project\gitee\xiaohai-blog\xiaohai-admin\target\xiaohai-blog.jar
+set JAR_FILE=D:\Project\gitee\xiaohai-blog\docker\admin\xiaohai-blog.jar
 
 rem 设置日志文件夹路径
-set LOG_FOLDER=D:\Project\gitee\xiaohai-blog\xiaohai-admin\target\logs
+set LOG_FOLDER=D:\Project\gitee\xiaohai-blog\docker\admin\logs
 
 rem 设置YML配置文件路径
-set YML_FILE=--spring.config.additional-location=D:\Project\gitee\xiaohai-blog\doc\logs\application.yml
+set YML_FILE=--spring.config.additional-location=D:\Project\gitee\xiaohai-blog\docker\admin\application.yml
 
-rem 设置 Java 启动参数 添加启动参数--initDatabase执行数据库初始化,注意进初始化使用该命令
+rem 设置 Java 启动参数
 set JAVA_OPTS=
 
 rem 获取当前日期和时间（格式：YYYYMMDD_HHMMSS）
