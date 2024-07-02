60b071cb495d2dce226242afefec3d400dce2272
wangchenghai <1372195290@qq.com>
2024-07-02 21:21:19 +0800

    添加tool页面

---
+++ xiaohai-web/src/views/system/tool/components/toLeadInto.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <el-dialog
+      title="Markdown 压缩包导入"
+      :visible.sync="leadInfo.show"
+      width="30%"
+    >
+      <el-upload
+        class="upload-demo"
+        drag
+        accept=".zip"
+        :limit="1"
+        :headers="{
+          'authorization':getToken()
+        }"
+        :action="fileAction"
+      >
+        <i class="el-icon-upload" />
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div slot="tip" class="el-upload__tip">
+          <p>压缩包中存在两个文件夹， <em style="color: red">image</em>（存放图片）和 <em style="color: red">note</em>（存放Markdown）</p>
+          <el-table size="small" :data="tableData">
+            <el-table-column align="center" label="MarkDown文件最上方以 --- 分隔的区域参数">
+              <el-table-column prop="key" label="参数" width="90px" />
+              <el-table-column prop="value" label="样例" />
+              <el-table-column prop="name" label="描述" />
+            </el-table-column>
+          </el-table>
+          请严格按照模板导入，只能上传zip文件，且不超过10M
+          <el-button type="text" @click="deriveFile('markdown导入压缩包模板.zip')">下载模板</el-button>
+        </div>
+      </el-upload>
+
+    </el-dialog>
+  </div>
+</template>
+<script>
+import { deriveFile } from '@/utils'
+import { getToken } from '@/utils/auth'
+
+export default {
+  name: 'ToleadInfo',
+  props: ['leadInfo'],
+  data() {
+    return {
+      tableData: [
+        {
+          key: 'title',
+          value: 'Chrome书签手动同步方法',
+          name: '文章标题'
+        },
+        {
+          key: 'date',
+          value: '2023-04-10 21:55:09',
+          name: '写作时间'
+        },
+        {
+          key: 'tags',
+          value: '[浏览器,Chrome]',
+          name: '标签列表'
+        },
+        {
+          key: 'categories',
+          value: '浏览器',
+          name: '分类属性仅为一个'
+        },
+        {
+          key: 'cover',
+          value: '../image/1684113802808.jpg',
+          name: '封面图，不上传默认获取bing图片'
+        }
+      ],
+      fileAction: process.env.VUE_APP_BASE_API + '/note/article/markdown'
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    getToken,
+    deriveFile
+  }
+}
+</script>
+<style scoped>
+::v-deep .el-upload,
+::v-deep .el-upload-dragger {
+  width: 100%;
+}
+</style>
---
+++ xiaohai-web/src/views/system/tool/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="app-container">
+    <el-row :gutter="20">
+      <el-col :span="5">
+        <el-card class="box-card min-height">
+          <div slot="header" class="clearfix">
+            <span>系统备份</span>
+          </div>
+          <p class="text">支持 Front Matter 数据导入解析</p>
+          <div class="float-right">
+            <el-button
+              icon="el-icon-upload"
+              size="small"
+              @click="handleImport"
+            >导 入
+            </el-button>
+          </div></el-card>
+      </el-col>
+      <el-col :span="5">
+        <el-card class="box-card min-height">
+          <div slot="header" class="clearfix">
+            <span>Markdown 文章导出</span>
+          </div>
+          <p class="text">支持 Front Matter 数据导入解析</p>
+          <div class="float-right">
+            <el-button
+              icon="el-icon-upload"
+              size="small"
+              @click="handleImport"
+            >导 入
+            </el-button>
+          </div></el-card>
+      </el-col>
+      <el-col :span="5">
+        <el-card v-if="$store.getters.permission.includes('note:article:import')" class="box-card min-height">
+          <div slot="header" class="clearfix">
+            <span>Markdown 文章导入</span>
+          </div>
+          <div>
+            <p class="text">支持 Front Matter 数据导入解析</p>
+          </div>
+          <div class="float-right">
+            <el-button
+              icon="el-icon-upload"
+              size="small"
+              @click="handleImport"
+            >导 入
+            </el-button>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <to-lead-into v-if="leadInfo.show" :lead-info="leadInfo" />
+  </div>
+</template>
+
+<script>
+import ToLeadInto from '@/views/system/tool/components/toLeadInto.vue'
+
+export default {
+  name: 'Index',
+  components: { ToLeadInto },
+  data() {
+    return {
+      leadInfo: {
+        show: false
+      }
+    }
+  },
+  created() {
+
+  },
+  methods: {
+    // 导入
+    handleImport() {
+      this.leadInfo.show = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.min-height {
+  min-height: 50px;
+  padding-bottom: 10px;
+  margin-bottom: 20px
+}
+.text {
+  text-align: center;
+  font-size: 14px;
+}
+.float-right {
+  float: right;
+  bottom: 0
+}
+</style>
